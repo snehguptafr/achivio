06d7060107f0067406a30b2fca8f245c6b6f1f6a
shraaavss <shravs2707@gmail.com>
2023-07-15 00:07:26 +0530

    footer revised

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,40 +18,34 @@ export default function Footer() {
 
       <section className="us">
 
-        <div id="shravya">
+        <div id="shravya" className="people">
+          <img src={shravs} alt=" " />
 
           <span className="group">
-            <img src={shravs} alt=" " />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum laborum suscipit dolorum dolor,
-              animi autem aliquam. Fuga, rem? Animi assumenda deleniti inventore nostrum quibusdam nam illo architecto?
-              Aspernatur, quibusdam ut.</p>
+            <p>Shravya</p>
+            <p className="socials">
+              <a href="https://github.com/shraaavss"><img className="social-icon" src={github} alt="" /></a>
+              <a href="https://www.linkedin.com/in/shravya-rao/"><img className="social-icon" src={linkd} alt="" /></a>
+              <a href="https://www.instagram.com/shraaavss/"><img className="social-icon" src={insta} alt="" /></a>
+              <a href="mailto:shravys2707@gmail.com"><img className="social-icon" src={mail} alt="" /></a>
+            </p>
           </span>
-
-          <p className="socials">
-            <a href="https://github.com/shraaavss"><img className="social-icon" src={github} alt="" /></a>
-            <a href="https://www.linkedin.com/in/shravya-rao/"><img className="social-icon" src={linkd} alt="" /></a>
-            <a href="https://www.instagram.com/shraaavss/"><img className="social-icon" src={insta} alt="" /></a>
-            <a href="mailto:shravys2707@gmail.com"><img className="social-icon" src={mail} alt="" /></a>
-          </p>
 
         </div>
 
 
-        <div id="sneh">
+        <div id="sneh" className="people">
+          <img src={sneh} alt=" " />
 
           <span className="group">
-            <img src={sneh} alt=" " />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum laborum suscipit dolorum dolor,
-              animi autem aliquam. Fuga, rem? Animi assumenda deleniti inventore nostrum quibusdam nam illo architecto?
-              Aspernatur, quibusdam ut.</p>
+            <p>Sneh</p>
+            <p className="socials">
+              <a href="https://github.com/snehguptafr"><img className="social-icon" src={github} alt="" /></a>
+              <a href="https://www.linkedin.com/in/sneh-gupta/"><img className="social-icon" src={linkd} alt="" /></a>
+              <a href="https://www.instagram.com/snehguptafr/"><img className="social-icon" src={insta} alt="" /></a>
+              <a href="mailto:guptasneh302004@gmail.com"><img className="social-icon" src={mail} alt="" /></a>
+            </p>
           </span>
-
-          <p className="socials">
-            <a href="https://github.com/snehguptafr"><img className="social-icon" src={github} alt="" /></a>
-            <a href="https://www.linkedin.com/in/sneh-gupta/"><img className="social-icon" src={linkd} alt="" /></a>
-            <a href="https://www.instagram.com/snehguptafr/"><img className="social-icon" src={insta} alt="" /></a>
-            <a href="mailto:guptasneh302004@gmail.com"><img className="social-icon" src={mail} alt="" /></a>
-          </p>
 
         </div>
 
