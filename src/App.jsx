ff26decad1ac736d60bc7523da2b3ee12b058692
shraaavss <shravs2707@gmail.com>
2023-07-15 01:36:38 +0530

    resize warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import CreateRoadmap from "./pages/CreateRoadmap";
 import ViewRoadmap from "./pages/ViewRoadmap";
@@ -7,6 +7,16 @@ import ShowRoadmaps from "./pages/ShowRoadmaps";
 import Certificate from "./pages/Certificate";
 
 export default function App(){
+
+    function checkScreenSize() {
+        if (window.innerWidth < 1200) {
+          alert('Please try this web app on a laptop for the best experience.');
+        }
+      }
+      
+      window.addEventListener('load', checkScreenSize);
+      window.addEventListener('resize', checkScreenSize);
+      
 
     return(
         <BrowserRouter>
