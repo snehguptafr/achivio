5e1cec52b906fea413186245851860ec1788f56b
snehguptafr <guptasneh302004@gmail.com>
2023-07-14 16:05:54 +0530

    added "show" route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateRoadmap from "./pages/CreateRoadmap";
 import ViewRoadmap from "./pages/ViewRoadmap";
+import ShowRoadmaps from "./pages/ShowRoadmaps";
 
 export default function App(){
 
@@ -13,6 +14,7 @@ export default function App(){
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<CreateRoadmap />} />
             <Route path="/view/:id" element={<ViewRoadmap />} />
+            <Route path="/show" element={<ShowRoadmaps />} />
 
         </Routes>
         </BrowserRouter>
