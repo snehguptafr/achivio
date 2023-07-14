ff0f6f98e6fdafbe1c8b81a9d4ea1e45106b7712
snehguptafr <guptasneh302004@gmail.com>
2023-07-14 16:35:12 +0530

    Show roadmap names from localStorage

--- src/pages/ShowRoadmaps.jsx
+++ src/pages/ShowRoadmaps.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Header from "../components/Header";
 
 export default function ShowRoadmaps() {
-//   const [checkpoints, setCheckpoints] = React.useState([]); //state to store checkpoints
-//   const [initialNodes, setInitialNodes] = React.useState([]); // store flowchart nodes from checkpoints
-//   const [initialEdges, setInitialEdges] = React.useState([]); //store the path/connection of nodes in the flowchart
+  //   const [checkpoints, setCheckpoints] = React.useState([]); //state to store checkpoints
+  //   const [initialNodes, setInitialNodes] = React.useState([]); // store flowchart nodes from checkpoints
+  //   const [initialEdges, setInitialEdges] = React.useState([]); //store the path/connection of nodes in the flowchart
+
+  const roadmaps = Object.keys(localStorage).map((roadmap) => {
+    return(
+      <div key={Object.keys(localStorage).indexOf(roadmap)} className="roadmap-card">
+        <h1>{roadmap}</h1>
+      </div>
+    )
+  })
+
+  return (
+    <>
+      <Header />
+      {roadmaps}
+    </>
+  );
 }
