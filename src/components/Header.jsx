e139a4eef827d347e79ea8754b7f42e0364e468d
snehguptafr <guptasneh302004@gmail.com>
2023-07-14 17:31:37 +0530

    Added logo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './css_files/Header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header>
-            <h1 className='app-logo'>#Achivio</h1>
+            <h1 className='app-logo' ><Link to={"/"}>#Achivio</Link></h1>
             <nav className='island-button'>
 
                 <a href="#ABOUT"><button className='contactBtn'>About</button></a>
