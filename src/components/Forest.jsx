2fcd00ade4ac209ae3c372dc31e5c0fd01de33b2
shraaavss <shravs2707@gmail.com>
2023-07-15 18:55:21 +0530

    disco btn made

--- src/components/Forest.jsx
+++ src/components/Forest.jsx
@@ -64,7 +64,7 @@ const Forest = ({ userRoadmap }) => {
   return (
     <>
       <div className="forest">
-        <button className={clearedCheckpoints === roadmap.length ? "Flashy-btn" : 'contactBtn'} onClick={clearedCheckpoints===roadmap.length ? () => navigate(`/certificate/${userRoadmap}`) 
+        <button className={clearedCheckpoints === roadmap.length ? "discoBtn" : 'contactBtn'} onClick={clearedCheckpoints===roadmap.length ? () => navigate(`/certificate/${userRoadmap}`) 
         : handleCheckpointCompletion}>
           {clearedCheckpoints===roadmap.length ? 'Get Certificate!' : 'Complete Checkpoint'}
         </button>
