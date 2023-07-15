8f4abacd98ca208fef3076f9ae1fa5a6ae8c5f54
snehguptafr <guptasneh302004@gmail.com>
2023-07-15 18:25:18 +0530

    Add overlay component

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./css_files/Overlay.css"
+const OverlayComponent = () => {
+  return (
+    <div className="overlay">
+      <h1>HYYY</h1>
+    </div>
+  );
+};
+
+export default OverlayComponent;
