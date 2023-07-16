a1319d6068ef2f6e6ae615f5d2dbfd0fa17678e7
shraaavss <shravs2707@gmail.com>
2023-07-16 10:33:31 +0530

    confetti added on overlay

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import "./css_files/Overlay.css"
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+
 const Overlay = ({ message }) => {
+
+  const { width, height } = useWindowSize()
+
   return (
     <div className="overlay">
+
+      <Confetti
+        width={width}
+        height={height}
+      />
+
       <h1>{message}</h1>
     </div>
   );
